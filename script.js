ea3fe1b7dfbe007152ae781b8fefe667950d3b25
Keishizkz7 <78611433+Keishizkz7@users.noreply.github.com>
2024-01-25 23:06:55 +0900

    Add files via upload
    Adding index.html, style.css & script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+function compute() {
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value; 
+  var interest = principal * years * rate / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  var amount = parseInt(principal) + parseFloat(interest);
+  var result = document.getElementById("result");
+  
+  if (principal <= 0) {
+      alert('Please enter a positive number!');
+      document.getElementById("principal").focus();
+  }
+
+//     If you deposit $[PRINCIPAL],<br>
+// at an interest rate of [RATE]%.<br>
+// You will receive an amount of $[INTEREST],<br>
+// in the year [YEAR]<br></br>
+  else {
+      result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+  } 
+}
+
+function updateRate()
+{
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+}
